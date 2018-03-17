6f74ffae6b0199ea1fcc04f79be80ee6eeb52bd5
kirknie <32438446+kirknie@users.noreply.github.com>
2018-03-16 18:42:06 -0700

    Integrate more functions to the rational function class

--- vecfit/rational_fct.py
+++ vecfit/rational_fct.py
@@ -7,6 +7,7 @@ Created on Tue Mar 13 16:31:51 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
+from .vector_fitting import calculate_zero
 
 
 class RationalFct:
@@ -76,5 +77,79 @@ class RationalFct:
 
         plt_fct(x, y, **kwargs)
         plt.grid(True, linestyle='--')
+
+    def zero(self):
+        if self.const:
+            return calculate_zero(self.pole, self.residue, self.const)
+        else:
+            raise RuntimeError('Do not have a constant term')
+
+    def bound(self, reflect, tau=0.2, f0=0):
+        bw = np.nan
+        if reflect == 0:
+            bound = -np.pi / 2 * (sum(1 / self.pole) + sum(1 / self.zero()))
+            bound = bound.real
+            if f0 != 0:
+                bw = bound / 2 / np.pi / np.log(1 / tau) * (2 * np.pi * f0) ** 2
+        elif reflect == np.inf:
+            bound = -np.pi / 2 * (sum(self.pole) + sum(self.zero()))
+            bound = bound.real
+            bw = bound / 2 / np.pi / np.log(1 / tau)
+        else:
+            raise RuntimeError('Bound calculation unsupported yet!')
+        return bound, bw
+
+    def bound_error(self, f, s, reflect, tau=0.2):
+        f_fit = self.model(s)
+        f_error = f_fit - f
+        # Calculate rho first (single load)
+        rho = (2 * np.abs(f_error)) / (1 - np.power(np.abs(f), 2))
+        int_fct = f_integral(s.imag, reflect) / 2 * np.log(1 + (1 - tau ** 2) / tau ** 2 * rho)
+        # delta_b = np.sum((int_fct[:-1] + int_fct[1:]) / 2 * (s.imag[1:] - s.imag[:-1]))
+        delta_b = num_integral(s.imag, int_fct)
+        return delta_b
+
+    def bound_integral(self, s, reflect):
+        f = f_integral(s.imag, reflect) * np.log(1/self.model(s))
+        return num_integral(s.imag, f)
+
+    def plot_improved_bound(self, real_limit, imag_limit, ax=None):
+        fig = plt if ax is None else ax
+
+        sample = 2000
+        x = np.linspace(-real_limit, 0, sample)
+        y = np.linspace(-imag_limit, imag_limit, 2 * sample)
+        xv, yv = np.meshgrid(x, y, sparse=False, indexing='xy')
+        s = xv + 1j * yv
+        f = self.model(s)
+        zero = self.zero()
+
+        # do the plot
+        fig.contourf(x, y, 2 - np.abs(f), [1, 2])
+        fig.plot(self.pole.real, self.pole.imag, 'x')
+        fig.plot(zero.real, zero.imag, 'o')
+        fig.axis([-real_limit, 0, -imag_limit, imag_limit])
+        fig.xlabel('Re\{s\}')
+        fig.ylabel('Im\{s\}')
+
+        return s, f
+
+
+def f_integral(w, reflect):
+    if reflect == np.inf:
+        f = np.ones(len(w))
+    elif reflect.real > 0.0:
+        f = 1/2 * (1/(reflect - 1j*w) + 1/(reflect + 1j*w)).real
+    elif reflect.real == 0.0:
+        f = 1/2 * (np.power(reflect.imag - w, -2) + np.power(reflect.imag - w, -2))
+    else:
+        raise RuntimeError('Invalid reflection point!')
+    return f
+
+
+def num_integral(x, y):
+    return np.sum((y[:-1] + y[1:]) / 2 * (x[1:] - x[:-1]))
+
+
 
 
