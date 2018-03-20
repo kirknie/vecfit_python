a865a555a5d7438318ba0fc3580206bc718e0746
kirknie <32438446+kirknie@users.noreply.github.com>
2018-03-19 17:00:23 -0700

    Add the vecfit package setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 16:12:51 2018
+
+@author: kirknie
+"""
+
+from setuptools import setup
+
+setup(name='vecfit',
+      version='0.1',
+      description='Generate csv file from OTA raw file, also plot near and far field pattern.',
+      url='https://github.com/kirknie/vecfit_python',
+      author='Ding Nie',
+      author_email='kirknie@gmail.com',
+      license='',
+      packages=['vecfit'],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+      zip_safe=False)
